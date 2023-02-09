952e614fafe1699439d6ad30ca613441e898d0b0
FatimaMuhammadAdam <98468845+FatimaMuhammadAdam@users.noreply.github.com>
2023-02-09 21:47:12 +0100

    Update day_13_list_com_excersise.py

--- 30Days of python/day_13/day_13_list_com_excersise.py
+++ 30Days of python/day_13/day_13_list_com_excersise.py
@@ -1,6 +1,10 @@
 #Filter only negative and zero in the list using list comprehension
 #numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
-def filter_list():
-    numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
-    return [x for x in numbers if x<=0]
-
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+filter = [h for h in numbers if h<=0]
+print(filter)
+#Flatten the following list of lists of lists to a one dimensional list :
+#list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+flattened_list = [ h for row in list_of_lists for h in row]
+print(flattened_list)
